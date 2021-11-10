6ff2607b45e50d592c15590436ff457cde9f41dd
Your Name <you@example.com>
2021-11-10 08:16:58 -0600

    commit

---
+++ script.js
@@ -0,0 +1,56 @@
+function showfirst() {
+    console.log("testing")
+    const element = document.getElementById("mainContent")
+    element.innerHTML="this is the first principle"
+
+
+}
+function showsecond() {
+    console.log("testing")
+    const element = document.getElementById("mainContent")
+    element.innerHTML="this is the second principle"
+
+
+}
+function showthird() {
+    console.log("testing")
+    const element = document.getElementById("mainContent")
+    element.innerHTML="this is the third principle"
+
+
+}
+function showfourth() {
+    console.log("testing")
+    const element = document.getElementById("mainContent")
+    element.innerHTML="this is the fourth principle"
+
+
+}
+function showfifth() {
+    console.log("testing")
+    const element = document.getElementById("mainContent")
+    element.innerHTML="this is the fifth principle"
+
+
+}
+function showsixth() {
+    console.log("testing")
+    const element = document.getElementById("mainContent")
+    element.innerHTML="this is the sixth principle"
+
+
+}
+function showseventh() {
+    console.log("testing")
+    const element = document.getElementById("mainContent")
+    element.innerHTML="this is the seventh principle"
+
+
+}
+function showeighth() {
+    console.log("testing")
+    const element = document.getElementById("mainContent")
+    element.innerHTML="this is the eighth principle"
+
+
+}
